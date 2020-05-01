7b359e31861fd8cbe022f7ac159416ba9a1cd658
juandiego <juandiego9221@gmail.com>
2020-05-01 13:24:30 -0500

    creacion de metodos

--- src/main/java/pe/com/jdmm21/felix/bookshelf/service/tui/sample/ExampleServiceProxy.java
+++ src/main/java/pe/com/jdmm21/felix/bookshelf/service/tui/sample/ExampleServiceProxy.java
@@ -1,6 +1,16 @@
 package pe.com.jdmm21.felix.bookshelf.service.tui.sample;
 
+import java.util.Set;
+
+import pe.com.jdmm21.felix.bookshelf.inventory.api.Book;
+
 public interface ExampleServiceProxy {
+	String SCOPE = "book";
+	String[] FUNCTIONS = new String[] { "add", "search" };
+
 	String greeting(String name);
 
+	Set<Book> search(String username, String password, String attribute, String filter);
+
+	Set<Book> search(String username, String password, String attribute, int lower, int upper);
 }
--- src/main/java/pe/com/jdmm21/felix/bookshelf/service/tui/sample/ExampleServiceProxyImpl.java
+++ src/main/java/pe/com/jdmm21/felix/bookshelf/service/tui/sample/ExampleServiceProxyImpl.java
@@ -1,7 +1,11 @@
 package pe.com.jdmm21.felix.bookshelf.service.tui.sample;
 
+import java.util.Set;
+
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+
+import pe.com.jdmm21.felix.bookshelf.inventory.api.Book;
 
 public class ExampleServiceProxyImpl implements ExampleServiceProxy {
 
@@ -14,6 +18,18 @@ public class ExampleServiceProxyImpl implements ExampleServiceProxy {
 	public String greeting(String name) {
 		System.out.println("hello " + name);
 		return name;
+	}
+
+	@Override
+	public Set<Book> search(String username, String password, String attribute, String filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Book> search(String username, String password, String attribute, int lower, int upper) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
